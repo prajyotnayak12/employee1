30291bf9ebd7f3e6ea1ab9c230029cf12e2db7af
prajyotnayak12 <110683704+prajyotnayak12@users.noreply.github.com>
2024-04-01 10:32:28 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -32,11 +32,11 @@ class Program
 
         List<Employee> employees = new List<Employee>
         {
-            new Employee(1, "Manash", "Jd", 50000, new DateTime(2024, 4, 15)),
-            new Employee(2, "Rajat", "SD", 60000, new DateTime(2024, 4, 20)),
-            new Employee(3, "Ashutosh", "JD", 55000, new DateTime(2024, 4, 10)),
+            new Employee(1, "Manash", "Jd", 40000, new DateTime(2024, 4, 15)),
+            new Employee(2, "Rajat", "SD", 45000, new DateTime(2024, 4, 20)),
+            new Employee(3, "Ashutosh", "JD", 50000, new DateTime(2024, 4, 10)),
             new Employee(4, "Rakesh", "SALE", 48000, new DateTime(2024, 4, 5)),
-            new Employee(5, "Saloni", "JD", 52000, new DateTime(2024, 4, 12))
+            new Employee(5, "Saloni", "JD", 55000, new DateTime(2024, 4, 12))
         };
 
 
@@ -134,7 +134,7 @@ class Program
         Console.WriteLine(employeeWithLowestSalary);
 
 
-        UpdateEmployeePositionAndSalary(employeeWithLowestSalary, "SA", 55000);
+        UpdateEmployeePositionAndSalary(employeeWithLowestSalary, "SA", 65000);
 
 
         Console.WriteLine("\nEmployee with updation:");
@@ -149,7 +149,7 @@ class Program
         }
 
         Employee lowestPaidEmployee = employees[0];
-        foreach (var employee in employees)
+       foreach (va r employee in employees)
         {
             if (employee.Salary < lowestPaidEmployee.Salary)
             {
